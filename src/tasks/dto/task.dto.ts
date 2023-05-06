9b4168e6c89f9be1288be436509b3d79192a3894
MiguelGarcia98GitHub <miguelgarciard98@gmail.com>
2023-05-06 18:24:02 +0200

    added multiple validators to createTask and updateTask

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,30 @@
 import { TaskStatus } from "../task.entity"
+import {IsNotEmpty, IsString, MinLength, IsOptional, IsIn} from "class-validator"
 
 export class CreateTaskDto {
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(5)
     title: string
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(10)
     description: string    
 }
 
 export class UpdateTaskDto {
+    @IsString()
+    @MinLength(5)
+    @IsOptional()
     title?: string
+
+    @IsString()
+    @MinLength(10)
+    @IsOptional()
     description?: string
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
     status?: TaskStatus
 }
